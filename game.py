1ab5b4828a1ff15b98edb1965e02390b03facdc2
Skynse <austinamakyeansah@gmail.com>
2020-06-11 23:10:58 +0000

    Added comments

--- game.py
+++ game.py
@@ -5,13 +5,13 @@ print('type !exit to exit the game')
 words = list(open('words.txt','r').readlines())
 
 def guess():
-    tries = 6
-    word = random.choice(words).rstrip()
-    full = "_"*len(word)
-    full_bank = list(full)
-    while tries > 0:
-        print(states(tries-1))
-        if ''.join(full_bank) == word:
+    tries = 6 #initialize guesses
+    word = random.choice(words).rstrip() #remove trailing whitespaces from the chosen word
+    full = "_"*len(word) #create the fancy blanks
+    full_bank = list(full) #turn the blanks into a list for index editing
+    while tries > 0: # intialize while loop for the try counter
+        print(states(tries-1)) #print the state of hangman (hangmans body)
+        if ''.join(full_bank) == word: #if the full set of chosen words eqals the word picked, then ask for restart
             print('You won')
             inp = input('Do you want to play again Y/N').lower()
             if inp == "y":
@@ -21,12 +21,12 @@ def guess():
         print(''.join(full_bank))
         print(f'tries: {tries}')
         letter = input('letter: ')
-        if letter in [i for i in word]:
-            for x in range(0,len(list(word))):
-                if letter == list(word)[x]:
+        if letter in [i for i in word]: #initialize some logic if the letter is in the chosen word
+            for x in range(0,len(list(word))): #make a variable x that goes through the list according to it's index
+                if letter == list(word)[x]: # if the chosen letter equals the index x, then fullbank with index length x, gets changed to the letter
                     full_bank[x] = letter
         else:
-            tries -=1
+            tries -=1 #if the letter is not in the word, then our try count decreases by 1.
 
         if tries == 0 or letter=="!exit":
             print(f'The correct word was {word}')
